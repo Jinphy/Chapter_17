de8693358a5cf331f52c84d665ce6e2608911cfd
Jinphy <709584408@qq.com>
2017-07-23 14:42:32 +0800

    add hugo library to project,which is using AOP

--- app/src/main/java/com/example/jinphy/chapter_17/MainActivity.java
+++ app/src/main/java/com/example/jinphy/chapter_17/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.lang.reflect.Method;
+
+import hugo.weaving.DebugLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        method("hello","aspectj",10,true);
     }
 
+    @DebugLog public String method(String ar1, String ar2, int ar3, boolean ar4) {
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return ar1;
+    }
 }
